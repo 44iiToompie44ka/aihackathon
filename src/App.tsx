d072ccc69bfd1a8526352402c04082550d7fb31b
44iiToompie44ka <ilya.toompie@gmail.com>
2024-12-15 18:21:34 +0800

    ohoh

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,28 @@ import './App.css'
 import gamesList from './components/game_icon/games_list';
 
 function App() {
-  return (    <>
+  return (
+    <>
+    <div className='viewPort'>
     <header><h2>JetisuDigital AI Playground</h2></header>
       <PopularGamesList />
+    </div>
     
-
-      
   </>)
 }
 
 
 const PopularGamesList = () => (
-    
     <div>
       <h3 className='popularHeader'>Популярное</h3>
-      <p className='gamesArray'>
+        <p className='gamesArray'>
         {
             gamesList.map((item, index) => (
                 <div key={index}>{item}</div>
             ))
         }
         </p>
-        
-    </div>
-
-    
-    
-    
+    </div>  
 );
 export default App
 
