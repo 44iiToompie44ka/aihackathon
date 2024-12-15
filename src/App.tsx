43b6a4eeb1e4818f480a581383cb116442624057
44iiToompie44ka <ilya.toompie@gmail.com>
2024-12-15 15:24:35 +0800

    ohoh

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,21 @@ function App() {
 
   </>)
 }
-const gamesList = [GameIcon("https://preview.redd.it/gawr-gura-by-donnami-v0-sm167kcbd4xc1.jpeg?width=1080&crop=smart&auto=webp&s=67103e61039369e790ccda078a402b2f7ef93c78", 'title1'), 'List Item 2', 'List Item 3'];
+const gamesList = [
+GameIcon("https://preview.redd.it/gawr-gura-by-donnami-v0-sm167kcbd4xc1.jpeg?width=1080&crop=smart&auto=webp&s=67103e61039369e790ccda078a402b2f7ef93c78", 'title1'), 
+'List Item 2', 
+'List Item 3'];
 
 const UsingArrayMap = () => (
     
-    <div>
+    <div className='gamesArray'>
+      <p>
         {
             gamesList.map((item, index) => (
                 <div key={index}>{item}</div>
             ))
         }
+        </p>
     </div>
 );
 export default App
