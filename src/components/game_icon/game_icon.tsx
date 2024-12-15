1c7fa1b5ee06bb008fe58b1528f7dbde4f91119a
44iiToompie44ka <ilya.toompie@gmail.com>
2024-12-15 16:50:38 +0800

    ohoh

--- src/components/game_icon/game_icon.tsx
+++ src/components/game_icon/game_icon.tsx
@@ -2,7 +2,12 @@ import '../game_icon/game_icon.css';
 
 function GameIcon(imageLink: string, gameTitle: string){
     return (<>
-            <img className="gameImage" src={imageLink} alt="Logo" height={100} width={100} />
+            <img 
+            className="gameImage" 
+            src={imageLink} alt="Logo" 
+            height={100}
+            width={100} 
+            />
             <div className="gameTitle">
                 {gameTitle}
         </div>
